829b7458487fee82ccf1d3a3826dc6f10d7beb4d
maxcastaneda <max.castaneda@gmail.com>
2025-03-02 19:28:58 -0600

    continuamos con el progreso del curso!

---
+++ miniproyectos/buscador/buscador.py
@@ -0,0 +1,46 @@
+import os
+import datetime
+from os import system
+import re 
+import time
+
+# buscar archivos
+def getArchivos(carpeta):
+    archivos = []
+    rutas = []
+    for root, _, files in os.walk(carpeta):
+        for file in files:
+            rutas.append(os.path.join(root, file))
+            archivos.append(file)
+            print(os.path.join(root, file))
+    return rutas, archivos
+
+
+
+inicio = time.time()
+carpeta = "Mi_Gran_Directorio"
+rutas, archivos = getArchivos(carpeta)
+
+system('cls')
+print("-" * 60)
+print(f"Fecha de búsqueda: {datetime.datetime.today().strftime("%d/%m/%Y %H:%M:%S")}\n")
+print("ARCHIVO\t\t\t\tNRO. SERIE")
+print("-------\t\t\t\t----------")
+total = 0
+for archivo, ruta in zip(archivos, rutas):
+    
+    openfile = open(ruta, "r")
+    texto = openfile.read()    
+    
+    patron = r"N\w{3}-\d{5}"
+    busqueda = re.findall(patron, texto)
+    for match in busqueda:
+        print(f"{archivo}\t\t\t{match}")
+        total += 1
+
+    fin = time.time()
+
+print("\n")
+print(f"Numeros encontrados: {total}")
+print(f"Duracion de la búsqueda: {round(fin-inicio)} segundos")
+print("-" * 60)
--- regularExpressions/basicos.py
+++ regularExpressions/basicos.py
@@ -18,3 +18,62 @@ patron = r"(Kael|Jedi)\s(\w+)"
 busqueda = re.findall(patron, texto)
 for match in busqueda:
     print(match)
+
+# Ejercicio 1
+"""
+Crea una función llamada verificar_email para comprobar si una dirección de email es correcta, 
+que verifique si el email dado como argumento contiene "@" (entre el nombre de usuario y el dominio) 
+y finaliza en ".com" (aunque aceptando también casos que cuentan con un dominio adicional, tal como ".com.br" para el caso de un usuario de Brasil).
+Si se encuentra el patrón, la función debe finalizar mostrando en pantalla el mensaje "Ok", 
+pero si detecta que la frase no contiene los elementos indicados, debe informarle al usuario "La dirección de email es incorrecta" imprimiendo el mensaje.
+"""
+def verificar_email(email):
+    pattern = "\w+@\w+\.com?[\.\w+]"
+    if re.match(pattern, email):
+        return "Ok"
+    else:
+        return "La dirección de email es incorrecta"
+""""
+print(verificar_email("aaaa"))
+print(verificar_email("hola@yo.com.br"))
+print(verificar_email("hola@yo.xom"))
+print(verificar_email("usuario@host.com"))
+print(verificar_email("hola@yo.com.mx"))
+"""
+
+# Ejercicio 2
+"""
+Crea una función llamada verificar_saludo para verificar si una frase entregada como argumento inicia con la palabra "Hola". 
+Si se encuentra el patrón, la función debe finalizar mostrando el mensaje "Ok", pero si detecta que la frase no contiene "Hola", 
+debe informarle al usuario "No has saludado" imprimiendo el mensaje en pantalla.
+"""
+
+def verificar_saludo(frase):
+    pattern = r"^Hola.*"
+    if re.match(pattern, frase):
+        print ("Ok")
+    else:
+        print ("No has saludado")
+
+verificar_saludo("Hola como estas")
+verificar_saludo("fsdfdsfdsf")
+
+# Ejercicio 3
+"""
+Práctica Módulo RE 3
+El código postal de una región determinada se forma a partir de dos caracteres alfanuméricos y cuatro numéricos a continuación (ejemplo: XX1234). 
+Crea una función, llamada verificar_cp para comprobar si el código postal pasado como argumento sigue este patrón. 
+Si el patrón es correcto, mostrar al usuario el mensaje "Ok", de lo contrario: "El código postal ingresado no es correcto".
+"""
+
+def verificar_cp(cp):
+    pattern = r"^\w{2}\d{4}$"
+    if re.match(pattern, cp):
+        print ("Ok")
+    else:
+        print ("El código postal ingresado no es correcto")
+
+verificar_cp("AA1234")
+verificar_cp("AA12345")
+verificar_cp("A1234")
+verificar_cp("XX1122")
